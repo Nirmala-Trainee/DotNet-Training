3e05b51c01c963d974540733ceb95df394edddd6
Nirmala Kannoji <nirmalak@infinite.com>
2024-11-04 21:13:38 +0530

    Assignment

---
+++ Assignment_3/string_length/string_length/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            
+            string Input = Console.ReadLine();
+
+            int length = Input.Length;
+
+            Console.WriteLine("The length of the word is: " + length);
+            Console.ReadLine();
+        }
+       
+    }
+}
---
+++ Assignment_3/string_reverce/string_reverce/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_reverce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+
+            string Input = Console.ReadLine();
+
+            char[] charArray = Input.ToCharArray();
+            Array.Reverse(charArray);
+
+            string reversedWord = new string(charArray);
+
+            Console.WriteLine("Reversed word: " + reversedWord);
+
+            Console.ReadLine();
+
+        }
+    }
+}
