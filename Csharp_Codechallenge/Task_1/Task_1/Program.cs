27f50c4a352d7a98f08f9b6a40e497c7d8c85b15
Nirmala Kannoji <nirmalak@infinite.com>
2024-11-06 11:16:28 +0530

    codechallenge

---
+++ Csharp_Codechallenge/Task_1/Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Enter the position to remove:");
+            int position = int.Parse(Console.ReadLine());
+
+            if (position < 0 || position >= input.Length)
+            {
+                Console.WriteLine("Invalid position!");
+            }
+            else
+            {
+                string result = input.Remove(position, 1);
+                Console.WriteLine("Resulting string: " + result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
---
+++ Csharp_Codechallenge/Task_3/Task_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int largest = num1;
+
+            if (num2 > largest)
+            {
+                largest = num2;
+            }
+
+            if (num3 > largest)
+            {
+                largest = num3;
+            }
+
+            Console.WriteLine($"The largest number among {num1}, {num2}, and {num3} is {largest}.");
+            Console.ReadLine();
+        }
+    }
+}
+
