7f6c3eb74c1e4b9af8f5aff0267cf14b46fafae7
Nirmala Kannoji <nirmalak@infinite.com>
2024-11-10 15:15:29 +0530

    Assignment 4 submitted

---
+++ Csharp Assignments/Assignments-4/Assignments-4/User_def_Exception.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class InsufficientBal_Exception : ApplicationException
+    {
+        public InsufficientBal_Exception(string msg) : base(msg)
+        {
+
+        }
+    }
+    class ExceptionHandling
+    {
+        public int Amount;
+        public int Balance;
+        public string Transaction_type;
+
+        public ExceptionHandling(int Amo, int Bal, string Trans_type)
+        {
+            Amount = Amo;
+            Balance = Bal;
+            Transaction_type = Trans_type;
+        }
+        public void Deposit(int Amount, int Balance)
+        {
+            Balance = Balance + Amount;
+            Console.WriteLine("The Balance after deposit amount{0} is {1}", Amount, Balance);
+        }
+
+        public void Withdraw(int Amount, int Balance)
+        {
+            try
+            {
+                if (Amount > Balance)
+                {
+                    throw (new InsufficientBal_Exception("Does not have sufficient balance"));
+                }
+                else
+                {
+                    Balance = Balance - Amount;
+                    Console.WriteLine("The Balance after withdraw amount{0} is {1}", Amount, Balance);
+                }
+            }
+            catch (InsufficientBal_Exception insufficient_balance)
+            {
+                Console.WriteLine("Error :" + insufficient_balance.Message);
+            }
+        }
+        public void Update_Balance(string Transc_type)
+        {
+            if (Transc_type == "d" || Transc_type == "D")
+            {
+                Deposit(Amount, Balance);
+            }
+            else if (Transc_type == "w" || Transc_type == "W")
+            {
+                Withdraw(Amount, Balance);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the balance");
+            int Bal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the amount");
+            int Amo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Transaction type");
+            string Trans_type = Console.ReadLine();
+
+            ExceptionHandling obj = new ExceptionHandling(Amo, Bal, Trans_type);
+            obj.Update_Balance(Trans_type);
+            Console.Read();
+        }
+
+    }
+}
---
+++ Csharp Assignments/Assignments-4/Assignments-4/doctor.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_4
+{
+    class Doctor
+    {
+        int regNo;
+        string name;
+        double fees_charged;
+
+        public int RegNo
+        {
+            get
+            {
+                return regNo;
+            }
+            set
+            {
+                regNo = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double Fees_Charged
+        {
+            get
+            {
+                return fees_charged;
+            }
+            set
+            {
+                fees_charged = value;
+            }
+        }
+
+    }
+
+    class Book
+    {
+        string Book_Name;
+        string Author_name;
+
+        public Book(string book_name, string author_name)
+        {
+            Book_Name = book_name;
+            Author_name = author_name;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Book Name : {0}", Book_Name);
+            Console.WriteLine("Author Name :{0} ", Author_name);
+        }
+    }
+    class Bookshelf
+    {
+        Book[] bobj = new Book[5];
+        public Book this[int pos]
+        {
+            get
+            {
+                return bobj[pos];
+            }
+            set
+            {
+                bobj[pos] = value;
+            }
+        }
+    }
+
+    class Driver
+    {
+        public static void Main()
+        {
+            Console.WriteLine("-----Enter Doctor Details------");
+            Console.WriteLine("Enter the Registration Number");
+            int reg_no = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Name");
+            string names = Console.ReadLine();
+            Console.WriteLine("Fees charged");
+            double fees = Convert.ToDouble(Console.ReadLine());
+            Doctor d = new Doctor();
+            Console.WriteLine("-----Displayiing Doctor class Details-----");
+            d.RegNo = reg_no;
+            Console.WriteLine("Registration Number: {0} ", d.RegNo);
+            d.Name = names;
+            Console.WriteLine("Name: {0}", d.Name);
+            d.Fees_Charged = fees;
+            Console.WriteLine("Fees charged: {0}", d.Fees_Charged);
+
+            Console.WriteLine("-----Enter Book class Details-------");
+            Console.WriteLine("Enter the book name");
+            string bname = Console.ReadLine();
+            Console.WriteLine("Enter the author name");
+            string aname = Console.ReadLine();
+            Book b = new Book(bname, aname);
+            Console.WriteLine("------Displaying Book class Details------");
+            b.Display();
+
+            Console.WriteLine("--Indexers Example--- ");
+            Bookshelf bs = new Bookshelf();
+            Console.WriteLine("---Entering Book Details---- ");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter the {0} book name ", i + 1);
+                string bookname = Console.ReadLine();
+                Console.WriteLine("Enter the {0} author name", i + 1);
+                string authorname = Console.ReadLine();
+                bs[i] = new Book(bookname, authorname);
+            }
+            Console.WriteLine("---Displaying Book details----");
+            for (int j = 0; j < 5; j++)
+            {
+                bs[j].Display();
+            }
+            Console.Read();
+        }
+    }
+}
