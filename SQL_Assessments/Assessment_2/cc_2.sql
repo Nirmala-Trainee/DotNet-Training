3898730e0453333464f6a1b1a8cf70990323bd7f
Nirmala Kannoji <nirmalak@infinite.com>
2024-12-04 11:16:28 +0530

    Assessment 2 Sql

---
+++ SQL_Assessments/Assessment_2/cc_2.sql
@@ -0,0 +1,88 @@
+use SQL
+
+--1-------
+declare @Birthday date = '2001-03-15'; 
+select @Birthday as 'Birthday',
+DATENAME(weekday, @Birthday) as 'Day of Week';
+
+----2--------
+declare @birthdate date = '2001-03-15'; 
+select DATEDIFF(DAY, @birthdate, GETDATE()) as AgeInDays;
+
+-----3-----
+select * from EMP
+ update EMP set Hiredate='2021-12-04' where Empno=7369; 
+  update EMP set Hiredate='2008-12-08' where Empno=7654; 
+   update EMP set Hiredate='2001-12-04' where Empno=7782; 
+    update EMP set Hiredate='2010-12-18' where Empno=7844; 
+
+	select * from EMP
+where year(getdate()) - year(hiredate) > 5
+AND MONTH(Hiredate) = MONTH(GETDATE());
+
+-----4-----
+create table Employe (
+    empno int primary key,
+    ename varchar(50),
+    sal decimal(10, 2),
+    doj date
+);
+
+-- 4a. Insert 3 rows
+insert into Employe (empno, ename, sal, doj) values (1, 'Swetha', 50000, '2022-01-15');
+insert into Employe (empno, ename, sal, doj) values (2, 'Arthi', 60000, '2021-02-20');
+insert into Employe (empno, ename, sal, doj) values (3, 'Ramya', 70000, '2020-03-10');
+
+select * from Employe
+ 
+ -----4b------
+ update Employe
+set sal = sal * 1.15
+where empno = 2;
+select * from Employe
+
+-- 4c. Delete the first row -------
+delete from Employe
+where empno = 1;
+
+select * from Employe
+
+-- To recall the deleted row, re-insert it using the deleted data
+insert into Employe values (1, 'Swetha', 50000, '2022-01-15');
+
+
+-----5------
+select * from EMP
+
+create function calculateBonus (@Deptno int, @Sal decimal(10, 2))
+returns decimal(10, 2)
+as
+begin
+    declare @bonus decimal(10, 2);
+    if @Deptno = 10
+        set @bonus = @sal * 0.15;
+    else if @Deptno = 20
+        set @bonus = @sal * 0.20;
+    else
+        set @bonus = @sal * 0.05;
+    return @bonus;
+end;
+
+select Empno, Ename, Sal, Deptno,
+    dbo.calculateBonus(Deptno, sal) as Bonus
+from EMP
+   
+------6-------------
+
+select * from DEPT
+
+create procedure UpdateSalaryForSales
+as
+begin
+  update EMP
+    set Sal = Sal + 500
+    where Deptno = (select Deptno from DEPT where Dname = 'Sales') AND Sal < 1500;
+end;
+  
+  Exec UpdateSalaryForSales
+  select *from EMP
