f062418f35bbbf6728285c78accb0d68e4dec5b9
Nirmala Kannoji <nirmalak@infinite.com>
2025-01-02 21:34:53 +0530

    MVS Assessment

---
+++ MVC_Assessment/Assessment_MVC_DBFirst/Controllers/CodeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment_1;
+
+namespace Assessment_1.Controllers
+{
+    public class CodeController : Controller
+    {
+            private northwindEntities db = new northwindEntities(); 
+
+            // Return all customers residing in Germany
+            public ActionResult CustomersInGermany()
+            {
+            var Customers = db.Customers.Where(customers => customers.Country == "Germany").ToList();
+            return View(Customers);
+        }
+
+            //Return customer details with a specific OrderId (e.g., 10248)
+            public ActionResult CustomerByOrderId(int orderId)
+            {
+                var customer = db.Orders
+                                 .Where(o => o.OrderID == orderId)
+                                 .Select(o => o.Customer)
+                                 .FirstOrDefault();
+                return View(customer);
+            }
+    }
+}
--- MVC_Assessment/MVC_Assessment_DB_First/MVC_Assessment_DB_First/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Assessment_DB_First.MvcApplication" Language="C#" %>
