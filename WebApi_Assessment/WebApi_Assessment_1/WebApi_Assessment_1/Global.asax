2e4617b92edeaf8fed53297ae1ceb07ac98b0b3f
Nirmala Kannoji <nirmalak@infinite.com>
2025-01-08 10:56:58 +0530

    WebApi Assessment

---
+++ WebApi_Assessment/WebApi_Assessment_1/WebApi_Assessment_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi_Assessment_1.WebApiApplication" Language="C#" %>
---
+++ WebApi_Assessment/WebApi_Assessment_1/WebApi_Assessment_1/Models/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_Assessment_1.Models
+{
+    public class Country
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string CountryName { get; set; }
+
+        [Required]
+        public string Capital { get; set; }
+    }
+}
